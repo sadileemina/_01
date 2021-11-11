1dc247254aad0dc16a2cfa17d041549b18e66452
sadileemina <94099600+sadileemina@users.noreply.github.com>
2021-11-11 16:12:02 +0900

    pxt.json, main.blocks, main.ts 업데이트하기

--- main.ts
+++ main.ts
@@ -2,6 +2,14 @@ input.onButtonPressed(Button.A, function () {
     basic.showIcon(IconNames.Heart)
     soundExpression.giggle.playUntilDone()
 })
+input.onButtonPressed(Button.AB, function () {
+    basic.showIcon(IconNames.Square)
+    music.startMelody(music.builtInMelody(Melodies.Birthday), MelodyOptions.Once)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.No)
+    soundExpression.soaring.playUntilDone()
+})
 basic.forever(function () {
 	
 })
